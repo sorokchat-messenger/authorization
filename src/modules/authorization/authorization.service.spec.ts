87c0dc357135863a9c00248ffcde6b20d8fa6679
Sorokovsky <Sorokovsky0809@gmail.com>
2026-09-13 21:27:31 +0300

    test: setup authorization service tests

--- src/modules/authorization/authorization.service.spec.ts
+++ src/modules/authorization/authorization.service.spec.ts
@@ -1,18 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthorizationService } from './authorization.service.js';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthorizationService } from "./authorization.service.js";
+import { UsersService } from "../users/users.service.js";
+import {
+  MOCK_PASSWORD_SECRET_PROVIDER,
+  MOCK_SIGNING_PROVIDER,
+  MOCK_USERS_REPOSITORY_PROVIDER,
+} from "../../../test/index.js";
 
-describe('AuthorizationService', () => {
+describe("AuthorizationService", () => {
   let service: AuthorizationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthorizationService],
+      providers: [
+        AuthorizationService,
+        MOCK_PASSWORD_SECRET_PROVIDER,
+        MOCK_SIGNING_PROVIDER,
+        MOCK_USERS_REPOSITORY_PROVIDER,
+        UsersService,
+      ],
     }).compile();
 
     service = module.get<AuthorizationService>(AuthorizationService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
